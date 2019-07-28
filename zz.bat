55f6f86b3403991afffddaca608c9d9c730fd563
Ashwani Yadav <ashyadavash@gmail.com>
2019-07-29 00:02:18 +0530

    changes

--- zz.bat
+++ zz.bat
@@ -1,3 +1,9 @@
+@echo off
+
+echo "Welcome to auto push"
+set /p branch="Enter branch: "
+set /p commit="Enter commit msg without spaces: "
+
 git add .
-git commit -m "new commit"
-git push origin issue1
+git commit -m %commit%
+git push origin %branch%
