63ea76e7a34ada8f5b8118cbca205af0ff8c74f6
Ashwani Yadav <ashyadavash@gmail.com>
2020-07-21 21:16:19 +0530

    Update config.js

--- configs/config.js
+++ configs/config.js
@@ -3,5 +3,7 @@ require('dotenv').config();
 module.exports={
     JWT_SECRET: process.env.JWT_SEC,
     DB_URI: process.env.DB_URI,
-    FCM_KEY: process.env.FCM_KEY
+    FCM_KEY: process.env.FCM_KEY,
+    MAIL_ID: process.env.MAIL_ID,
+    MAIL_PASS: process.env.MAIL_PASS
 }
